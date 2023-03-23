b30cc109c936b936bf964b8992f8289cd2efbabd
J8051 <nathaniel.jessica@yahoo.ca>
2023-03-23 10:43:34 -0400

    The Empty component

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
 
 //Button stories 
 storiesOf("Button", module)
@@ -149,4 +151,16 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm"/>);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+
+  //Empty Stories 
+storiesOf("Empty", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+ 
+
+
+
