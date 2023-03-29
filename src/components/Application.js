122aef2c7e5264f972d7c91d9ee8a28ad5b196f5
J8051 <nathaniel.jessica@yahoo.ca>
2023-03-29 10:57:42 -0400

    combining states

--- src/components/Application.js
+++ src/components/Application.js
@@ -16,7 +16,7 @@ const appointments = {
     time: "1pm",
     interview: {
       student: "Lydia Miller-Jones",
-      interviewer:{
+      interviewer: {
         id: 3,
         name: "Sylvia Palmer",
         avatar: "https://i.imgur.com/LpaY82x.png",
@@ -32,7 +32,7 @@ const appointments = {
     time: "3pm",
     interview: {
       student: "Archie Andrews",
-      interviewer:{
+      interviewer: {
         id: 4,
         name: "Cohana Roy",
         avatar: "https://i.imgur.com/FK8V841.jpg",
@@ -53,53 +53,59 @@ const appointments = {
 //Component Begins 
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
   
-  useEffect(() => { 
-    axios.get('http://localhost:8001/api/days')
-      .then((response) => {
-        console.log(response.data); 
-        setDays(response.data) 
-        
-       })
-    
-  },[])
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
 
-  const arrayOfAppointments = Object.values(appointments); 
-  const apps = arrayOfAppointments.map((appointment) => { 
 
-    return (
-      <Appointment
-        key={appointment.id} 
-        {...appointment}
-      />
-    )
-  })
+useEffect(() => {
+  axios.get('http://localhost:8001/api/days')
+    .then(response => setDays(response.data));
+}, []);
 
+
+const arrayOfAppointments = Object.values(appointments);
+const apps = arrayOfAppointments.map((appointment) => {
 
   return (
-    <main className="layout">
-
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay}/>
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-      </section>
-      <section className="schedule">
-        {apps}
-      </section>
-    </main>
+    <Appointment
+      key={appointment.id}
+      {...appointment}
+    />
   );
+});
+
+
+return (
+  <main className="layout">
+
+    <section className="sidebar">
+      <img
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
+      />
+      <hr className="sidebar__separator sidebar--centered" />
+      <nav className="sidebar__menu">
+        <DayList days={state.days} value={state.day} onChange={setDay} />
+      </nav>
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
+    </section>
+    <section className="schedule">
+      {apps}
+    </section>
+  </main>
+);
 }
