e6a2305d13d2944d273eaa855a1c8b98b107b2da
J8051 <nathaniel.jessica@yahoo.ca>
2023-04-17 16:07:39 -0400

    initial cypress test find a class name passes

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,3 +3,10 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 });
+describe("Navigation", () => {
+  it("should navigate to Tuesday", () => {
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected")
+  });
+});
